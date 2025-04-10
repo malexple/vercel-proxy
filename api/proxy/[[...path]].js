14e0cafc03744dcf0f105ad8835eb370ac2309a2
malexple <malexple@gmail.com>
2025-04-10 17:17:29 +0300

    change logic

--- api/proxy/[[...path]].js
+++ api/proxy/[[...path]].js
@@ -1,24 +1,13 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const proxy = createProxyMiddleware({
-  target: 'http://file.malexple.ru', // Замените на нужный URL
+  target: 'http://file.malexple.ru',
   changeOrigin: true,
-  pathRewrite: { '^/api/proxy': '' },
-  followRedirects: true,
 });
 
-export default function (req, res) {
-  // В код прокси (перед вызовом proxy())
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  console.log('Запрос к:', req.url); // Логировать URL
-  req.body = null;
-  proxy(req, res, (err) => {
-    if (err) {
-      console.error('Ошибка:', err);
-      res.status(500).end();
-    }
-  });
-}
+export default (req, res) => {
+  proxy(req, res);
+};
 
 export const config = {
   api: {
