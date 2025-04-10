0c1f55d9c6948625003168b723958d0eba69e9ef
malexple <malexple@gmail.com>
2025-04-10 16:16:20 +0300

    add site proxy

---
+++ api/proxy/[[...path]].js
@@ -0,0 +1,24 @@
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+const proxy = createProxyMiddleware({
+  target: 'https://file.malexple.ru', // Замените на нужный URL
+  changeOrigin: true,
+  pathRewrite: { '^/api/proxy': '' }, // Удаляет префикс пути (если нужно)
+  followRedirects: true,
+});
+
+export default function handler(req, res) {
+  req.body = null; // Отключаем парсинг тела запроса
+  proxy(req, res, (err) => {
+    if (err) {
+      res.status(500).end();
+    }
+  });
+}
+
+// Отключаем встроенный парсинг тела запроса
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
