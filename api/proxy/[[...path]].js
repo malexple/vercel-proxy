524470a96c9b09a55a3abff9c0582588d47f2de7
malexple <malexple@gmail.com>
2025-04-10 19:56:28 +0300

    small changes

--- api/proxy/[[...path]].js
+++ api/proxy/[[...path]].js
@@ -3,26 +3,42 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const proxy = createProxyMiddleware({
   target: 'http://file.malexple.ru',
   changeOrigin: true,
-  secure: false, // Отключаем проверку SSL (если нет HTTPS)
-  cookieDomainRewrite: 'localhost', // Важно для сессий
+  cookieDomainRewrite: {
+    '*': 'localhost', // Переписываем все домены в localhost
+  },
   onProxyReq: (proxyReq, req) => {
-    // Добавляем заголовки для обхода CSRF
+    // Добавляем CSRF-токен из куки в заголовок
+    if (req.cookies && req.cookies.csrftoken) {
+      proxyReq.setHeader('X-CSRFToken', req.cookies.csrftoken);
+    }
+
+    // Фиксим Referer для Django
     proxyReq.setHeader('Referer', 'http://file.malexple.ru');
-    proxyReq.setHeader('X-Forwarded-Host', 'file.malexple.ru');
   },
   onProxyRes: (proxyRes) => {
-    // Фиксим куки для локального хоста
+    // Фиксим Set-Cookie заголовки
     const cookies = proxyRes.headers['set-cookie'];
     if (cookies) {
       proxyRes.headers['set-cookie'] = cookies.map(cookie =>
-        cookie.replace(/Domain=file.malexple.ru;?/i, 'Domain=localhost;')
+        cookie
+          .replace(/Domain=file\.malexple\.ru;?/gi, 'Domain=localhost;')
+          .replace(/Secure;?/gi, '')
+          .replace(/SameSite=Lax;?/gi, 'SameSite=None; Secure')
       );
     }
   }
 });
 
 module.exports = (req, res) => {
-  proxy(req, res);
+  // Сохраняем куки из запроса
+  req.cookies = require('cookie').parse(req.headers.cookie || '');
+
+  proxy(req, res, (err) => {
+    if (err) {
+      console.error('Proxy error:', err);
+      res.status(500).end();
+    }
+  });
 };
 
 module.exports.config = {
