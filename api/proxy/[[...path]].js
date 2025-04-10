c28c77cd819be34ac111c489c007c330c8344bc5
malexple <malexple@gmail.com>
2025-04-10 16:28:10 +0300

    small changes

--- api/proxy/[[...path]].js
+++ api/proxy/[[...path]].js
@@ -3,12 +3,12 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 const proxy = createProxyMiddleware({
   target: 'http://file.malexple.ru', // Замените на нужный URL
   changeOrigin: true,
-  pathRewrite: { '^/api/proxy': '' }, // Удаляет префикс пути (если нужно)
+  pathRewrite: { '^/api/proxy': '' },
   followRedirects: true,
 });
 
-export default function handler(req, res) {
-  req.body = null; // Отключаем парсинг тела запроса
+export default function (req, res) {
+  req.body = null;
   proxy(req, res, (err) => {
     if (err) {
       res.status(500).end();
@@ -16,7 +16,6 @@ export default function handler(req, res) {
   });
 }
 
-// Отключаем встроенный парсинг тела запроса
 export const config = {
   api: {
     bodyParser: false,
