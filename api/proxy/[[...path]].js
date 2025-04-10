c2df855ba1b2cbe79011b7a31eb76c0d21238386
malexple <malexple@gmail.com>
2025-04-10 17:29:00 +0300

    small changes

--- api/proxy/[[...path]].js
+++ api/proxy/[[...path]].js
@@ -1,16 +1,27 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const proxy = createProxyMiddleware({
-  target: 'http://file.malexple.ru',
+  target: 'http://file.malexple.ru', // Или https://
   changeOrigin: true,
+  pathRewrite: { '^/api/proxy': '' }, // Удалите, если не нужно
+  onProxyRes: (proxyRes) => {
+    // Добавьте CORS-заголовки, если нужно
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  },
 });
 
-export default (req, res) => {
-  proxy(req, res);
-};
+export default function handler(req, res) {
+  proxy(req, res, (err) => {
+    if (err) {
+      console.error('Proxy error:', err);
+      res.status(500).end();
+    }
+  });
+}
 
 export const config = {
   api: {
     bodyParser: false,
+    externalResolver: true,
   },
 };
