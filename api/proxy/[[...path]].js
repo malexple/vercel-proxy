8642892e138b2207cfe8a82afd67096a34216061
malexple <malexple@gmail.com>
2025-04-10 17:01:32 +0300

    add log

--- api/proxy/[[...path]].js
+++ api/proxy/[[...path]].js
@@ -8,9 +8,13 @@ const proxy = createProxyMiddleware({
 });
 
 export default function (req, res) {
+  // В код прокси (перед вызовом proxy())
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  console.log('Запрос к:', req.url); // Логировать URL
   req.body = null;
   proxy(req, res, (err) => {
     if (err) {
+      console.error('Ошибка:', err);
       res.status(500).end();
     }
   });
