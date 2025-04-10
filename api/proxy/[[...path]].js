e3eb9de152a6177b0856d3ea75097f1a56e8d81f
malexple <malexple@gmail.com>
2025-04-10 19:20:44 +0300

    small changes

--- api/proxy/[[...path]].js
+++ api/proxy/[[...path]].js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const proxy = createProxyMiddleware({
-  target: 'https://file.malexple.ru', // Убедитесь, что URL корректен
+  target: 'http://file.malexple.ru', // Убедитесь, что URL корректен
   changeOrigin: true,
   pathRewrite: { '^/api/proxy': '' }, // Удалите, если не нужно
   onProxyRes: (proxyRes) => {
