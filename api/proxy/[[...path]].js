0f089d8be249e958ba64bc20d3733ebd9b012e19
malexple <malexple@gmail.com>
2025-04-10 19:27:01 +0300

    small changes

--- api/proxy/[[...path]].js
+++ api/proxy/[[...path]].js
@@ -1,3 +1,24 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const proxy = createProxyMiddleware({ target: 'https://google.com' });
-module.exports = (req, res) => proxy(req, res);
+
+const proxy = createProxyMiddleware({
+  target: 'https://www.google.com', // Убедитесь, что URL корректен
+  changeOrigin: true,
+  onProxyRes: (proxyRes) => {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*'; // Добавьте CORS
+  },
+});
+
+module.exports = (req, res) => {
+  proxy(req, res, (err) => {
+    if (err) {
+      console.error('Proxy error:', err);
+      res.status(500).end();
+    }
+  });
+};
+
+module.exports.config = {
+  api: {
+    bodyParser: false,
+  },
+};
